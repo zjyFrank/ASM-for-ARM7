f6be63c9a10c01bf415b65554ce02167231641fc
zjyFrank <851016507@qq.com>
2020-05-23 20:00:06 +0800

    add food

--- SNAKE.s
+++ SNAKE.s
@@ -1,68 +1,79 @@
 .arm
 .text
+@.extern myRandom  @调用c,用来生成伪随机数(1-m)
+@--------------------------------
+@   #include "stdlib.h"
+@   int myRandom(int m)
+@   {
+@      return 1 + rand() % m ;
+@   }
+@--------------------------------
+@ cmd : arm-uclinuxeabi-gcc.exe -Wall -S -mcpu=arm7tdmi myRandom.c
 
 .equ column,40
 .equ row,15
 .equ edge,'*'
 .equ head,'H'
 .equ body,'O'
-.equ blank,' '      @ 置为blank
+.equ blank,' '  @ 置为blank
+.equ food,'$'
 .equ up   ,1    @up键按下
 .equ down ,2    @down键按下
 .equ left ,3    @left键按下
 .equ right,4    @right键按下
-.equ BLUE_KEY_01, 0x02 @button(1)
-.equ BLUE_KEY_04, 0x10 @button(4)
-.equ BLUE_KEY_05, 0x20 @button(5)
-.equ BLUE_KEY_06, 0x40 @button(6)
+.equ BLUE_KEY_01, 0x02      @button(1)
+.equ BLUE_KEY_04, 0x10      @button(4)
+.equ BLUE_KEY_05, 0x20      @button(5)
+.equ BLUE_KEY_06, 0x40      @button(6)
+.equ SWI_GetTicks, 0x6d     @获取当前时间
 
 
 @ clear screen
     SWI 0x206
 
-@@打印地图
+@打印地图
 @---------------------------
-@Draw_map:
-@    @ top
-@    Draw_1st_line:
-@        MOV r3,#column
-@        BL Draw_row
-@    @ bottom
-@    Draw_end_line:
-@        MOV r3,#column
-@        MOV r0,#0
-@        MOV r1,#14
-@        BL Draw_row
-@    @ left
-@    Draw_left_side:
-@        MOV r0,#0
-@        MOV r1,#0
-@        MOV r3,#row
-@        BL Draw_column
-@    @ right
-@    Draw_right_side:
-@        MOV r0,#39
-@        MOV r1,#0
-@        MOV r3,#row
-@        BL Draw_column
-@
-@    B Init
-@    @ 画行子函数
-@    Draw_row:
-@        MOV r2,#edge
-@        SWI 0x207
-@        ADD r0,r0,#1
-@        SUBS r3,r3,#1
-@        BNE Draw_row
-@    MOV pc,lr
-@    @ 画列子函数
-@    Draw_column:
-@        SWI 0x207
-@        ADD r1,r1,#1
-@        SUBS r3,r3,#1
-@        BNE Draw_column
-@    MOV pc,lr
-@@-----------------------------
+Draw_map:
+    @ top
+    Draw_1st_line:
+        MOV r3,#column
+        BL Draw_row
+    @ bottom
+    Draw_end_line:
+        MOV r3,#column
+        MOV r0,#0
+        MOV r1,#14
+        BL Draw_row
+    @ left
+    Draw_left_side:
+        MOV r0,#0
+        MOV r1,#0
+        MOV r3,#row
+        BL Draw_column
+    @ right
+    Draw_right_side:
+        MOV r0,#39
+        MOV r1,#0
+        MOV r3,#row
+        BL Draw_column
+
+    B Init
+    @ 画行子函数
+    Draw_row:
+        MOV r2,#edge
+        SWI 0x207
+        ADD r0,r0,#1
+        SUBS r3,r3,#1
+        BNE Draw_row
+    MOV pc,lr
+    @ 画列子函数
+    Draw_column:
+        SWI 0x207
+        ADD r1,r1,#1
+        SUBS r3,r3,#1
+        BNE Draw_column
+    MOV pc,lr
+@-----------------------------
 
 @初始化
 Init:
@@ -88,24 +99,44 @@ Init:
         BNE Init_body 
     @打印shead坐标
     MOV r0,#2
-    MOV r1,#13
+    MOV r1,#14
     LDR r2,=Message2
     SWI 0x204   
     MOV r0,#10
-    MOV r1,#13
+    MOV r1,#14
     MOV r2,r5
     SWI 0x205
     MOV r0,#12
-    MOV r1,#13
+    MOV r1,#14
     MOV r2,#','
     SWI 0x207
     MOV r0,#13
-    MOV r1,#13
+    MOV r1,#14
     MOV r2,r6
     SWI 0x205
 
+    @打印score
+    MOV r0,#15
+    MOV r1,#14
+    LDR r2,=Message4
+    SWI 0x204
+
+    MOV r9,#3
+    MOV r0,#26
+    MOV r1,#14
+    SUB r2,r9,#3
+    SWI 0x205
     
-@主循环    
+    @打印用户
+    MOV r0,#28
+    MOV r1,#14
+    LDR r2,=Message5
+    SWI 0x204
+
+    BL Creat_food
+    
+@主循环
+@---------------------------------------------------------------    
     MOV r9,#3
 Main_loop:
     LDR r7,=sbodyx
@@ -168,30 +199,56 @@ Main_loop:
             MOV r2,#head
             SWI 0x207
 
-    @更新shead坐标
+    @吃食物判断
+    LDR r0,=foodx
+    LDR r1,=foody
+    Judge_foodX:
+        LDR r0,[r0]
+        CMP r5,r0
+        BEQ Judge_foody
+        B next1
+    Judge_foody:
+        LDR r1,[r1]
+        CMP r6,r1
+        ADDEQ r9,r9,#1      @蛇长+1
+        BLEQ Creat_food     @产生新的食物
+    next1:
+
+    @更新显示shead坐标
     MOV r0,#10
-    MOV r1,#13
+    MOV r1,#14
     LDR r2,=Message3
     SWI 0x204
 
     MOV r0,#13
-    MOV r1,#13
+    MOV r1,#14
     LDR r2,=Message3
     SWI 0x204  
 
     MOV r0,#10
-    MOV r1,#13
+    MOV r1,#14
     MOV r2,r5
     SWI 0x205   @sheadx
 
     MOV r0,#13
-    MOV r1,#13
+    MOV r1,#14
     MOV r2,r6
     SWI 0x205   @sheady
 
+    @更新显示分数
+    MOV r0,#26
+    MOV r1,#14
+    LDR r2,=Message3
+    SWI 0x204 
+
+    MOV r0,#26
+    MOV r1,#14
+    SUB r2,r9,#3
+    SWI 0x205
+
     @撞身体判断(sheadx==sbodyx && sheady==sbodyy)
     MOV r2,r9
-    SUB r2,r2,#1    @ R2 循环次数
+    SUB r2,r2,#1    @ r2 循环次数
     Touch_body:
             LDR r0,[r7]
             CMP r5,r0
@@ -218,14 +275,55 @@ Main_loop:
     CMP r6,#14
     BEQ Game_over
 
-    B Main_loop
+B Main_loop
+@--------------------------------MAIN END---------------------------
 
-B Game_over
+@--------------------------------FUNCTION---------------------------
+@生成食物子函数
+Creat_food:
+    @生成伪随机数
+    Creat_rand:
+        SWI SWI_GetTicks    @获取当前时间，作为伪随机数
+        AND r0,r0,#0xFF
+        MOV r1,r0
+        @ 取模
+        Modx:
+            CMP r1,#37
+            BLS Randx
+            SUBHI r1,r1,#38
+            B Modx
+        
+        Randx:
+            ADD r1,r1,#1
+            STMFD sp!,{r1}
 
+        SWI SWI_GetTicks    @获取当前时间，作为伪随机数
+        AND r0,r0,#0xFF
+        MOV r1,r0
+        @ 取模
+        Mody:
+            CMP r1,#12
+            BLS Randy
+            SUBHI r1,r1,#13
+            B Mody
+        Randy:
+            ADD r1,r1,#1    @ r1
+            LDMFD sp!,{r0}  @ r0
+
+    LDR r2,=foodx
+    STR r0,[r2]     @ r0 : 1-38的随机数
+
+    LDR r2,=foody
+    STR r1,[r2]     @ r1 : 1-13的随机数
+
+    MOV r2,#food
+    SWI 0x207
+MOV pc,lr
+
+    
 @延时子函数
 Delay:
-    @MOV r3,#0x5000  @时长
-    MOV r3,#1  @时长
+    MOV r3,#0x1  @时长
 Delay_loop:
     SWI 0x203       @轮询(查询)
     @判断按键
@@ -242,9 +340,9 @@ Delay_loop:
     CMP r0,#0
     BNE Update_tail
 
-    SWI 0x202
-    CMP r0,#0x02
-    ADDEQ r9,r9,#1
+@    SWI 0x202
+@    CMP r0,#0x02
+@    ADDEQ r9,r9,#1
 
     SUBS r3,r3,#1
     BNE Delay_loop
@@ -279,19 +377,17 @@ Game_over:
     LDR r2,=Message1
     SWI 0x204   @ display GAME OVER
 
-
-
-
-
 .data
 sbodyx: .word  9, 8, 7 
         .space 100
 sbodyy: .word  7, 7, 7
         .space 100
+foodx:  .word 0
+foody:  .word 0
 Message1:.asciz "GAME OVER !"
-Message2:.asciz "shead = "
+Message2:.asciz " head = "
 Message3:.asciz "  "
-
-
+Message4:.asciz " / SCORE = "
+Message5:.asciz "/ usr:ZJY "
 
 .end
